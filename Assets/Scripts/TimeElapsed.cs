59bafad4353b1294219bcb222f5268b9d6087aca
Staycia930 <skb.ceg.biblia@gmail.com>
2023-12-11 19:40:35 +0900

    コメントアウトを追加

--- Assets/Scripts/TimeElapsed.cs
+++ Assets/Scripts/TimeElapsed.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class TimeElapsed : MonoBehaviour
 {
     [SerializeField]
+    // 昼間の空のマテリアル
     public Material sky;
+    // 夜の空のマテリアル
     public Material night;
+    // スカイボックスを格納する変数
     private Material skyboxMaterial;
+    // 経過時間を格納する変数
     private float time = 0;
+    // 夜になったかどうかを判定する変数
     private bool tf = false;
     void Start()
     {
+        // スカイボックスを設定
         RenderSettings.skybox = Instantiate(sky);
         skyboxMaterial = RenderSettings.skybox;
     }
@@ -19,6 +25,7 @@ public class TimeElapsed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 時間経過で空を暗くしたり、夜になったりする
         // Time.sincelevelLoad += Time.deltaTime;
         time += Time.deltaTime;
         if (time < 10.0f) {
