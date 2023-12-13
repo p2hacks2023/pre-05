51073f4f3e044a39d424d02faf6ab65429a5bb7a
Staycia930 <skb.ceg.biblia@gmail.com>
2023-12-13 20:50:55 +0900

    パラメータにアクセス可能 内部時間をparameter.csに移行

--- Assets/Scripts/Parameter.cs
+++ Assets/Scripts/Parameter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Parameter : MonoBehaviour
 {
+    public float time = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Parameter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
     }
 }
--- Assets/Scripts/TimeElapsed.cs
+++ Assets/Scripts/TimeElapsed.cs
@@ -12,7 +12,8 @@ public class TimeElapsed : MonoBehaviour
     // スカイボックスを格納する変数
     private Material skyboxMaterial;
     // 経過時間を格納する変数
-    private float time = 0.0f;
+    private float time;
+    Parameter parameter;
     // 夜になったかどうかを判定する変数
     private bool tf = false;
     void Start()
@@ -20,22 +21,23 @@ public class TimeElapsed : MonoBehaviour
         // スカイボックスを設定
         RenderSettings.skybox = Instantiate(sky);
         skyboxMaterial = RenderSettings.skybox;
+        parameter = GameObject.Find("GameSystem").GetComponent<Parameter>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        // time += Time.deltaTime;
         // 時間経過で空を暗くしたり、夜になったりする
         // Time.sincelevelLoad += Time.deltaTime;
-        if (time < 10.0f) {
+        if (parameter.time < 10.0f) {
             var c = skyboxMaterial.GetColor("_Tint");
             c.r -= 0.0002f;
             c.g -= 0.0002f;
             c.b -= 0.0002f;
             skyboxMaterial.SetColor("_Tint", c);
         }
-        else if (time > 10.0f && !tf) {
+        else if (parameter.time > 10.0f && !tf) {
             tf = true;
             RenderSettings.skybox = Instantiate(night);
         }
