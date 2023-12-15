963a4388028784bc5851dd7678080a36d1105274
Staycia930 <skb.ceg.biblia@gmail.com>
2023-12-15 23:41:03 +0900

    timeControlの変更の同期

--- Assets/Scripts/TimeElapsed.cs
+++ Assets/Scripts/TimeElapsed.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class TimeElapsed : MonoBehaviour
 {
     [SerializeField]
-    // 昼間の空のマテリアル
-    public Material sky;
-    // 夜の空のマテリアル
-    public Material night;
-    // スカイボックスを格納する変数
-    private Material skyboxMaterial;
     // 経過時間を格納する変数
     private float time;
     Parameter parameter;
     // 夜になったかどうかを判定する変数
     private bool tf = false;
+    private int dayNum = 0;
+    public Camera camera;
+    public Light day;
+    public Light night;
+    public Light deskLight;
+    
+    // 0.5235849 0.8560204 1 1
     void Start()
     {
-        // スカイボックスを設定
-        RenderSettings.skybox = Instantiate(sky);
-        skyboxMaterial = RenderSettings.skybox;
         parameter = GameObject.Find("GameSystem").GetComponent<Parameter>();
+        day.enabled = true;
+        night.enabled = false;
+        deskLight.enabled = false;
+        camera.backgroundColor = new Color(0.5235849f, 0.8560204f, 1, 1);
     }
 
     // Update is called once per frame
@@ -30,16 +32,24 @@ public class TimeElapsed : MonoBehaviour
         // time += Time.deltaTime;
         // 時間経過で空を暗くしたり、夜になったりする
         // Time.sincelevelLoad += Time.deltaTime;
-        if (parameter.time < 10.0f) {
-            var c = skyboxMaterial.GetColor("_Tint");
-            c.r -= 0.0002f;
-            c.g -= 0.0002f;
-            c.b -= 0.0002f;
-            skyboxMaterial.SetColor("_Tint", c);
+        // 昼の時間と夜の時間はそれぞれ1分
+        if (dayNum == 0) {
+            if (parameter.time > 60f) {
+                dayNum = 1;
+                day.enabled = false;
+                night.enabled = true;
+                deskLight.enabled = true;
+                // camera.backgroundColor = new Color(0.6132076f, 0.2983172f, 0, 1);/
+                camera.backgroundColor = new Color(0.03918803f, 0, 0.2264151f, 1);
+            }
+        
         }
-        else if (parameter.time > 10.0f && !tf) {
-            tf = true;
-            RenderSettings.skybox = Instantiate(night);
+        if (dayNum == 1) {
+            if (parameter.time > 120f) {
+                dayNum = 2;
+                night.enabled = false;
+                camera.backgroundColor = new Color(0.03918803f, 0, 0.2264151f, 1);
+            }
         }
 	}
 }
