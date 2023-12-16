60d09efd3465d95fffe5eb08f1e88df1693308cc
Staycia930 <skb.ceg.biblia@gmail.com>
2023-12-16 15:16:38 +0900

    外に出るUIの作成

---
+++ Assets/Scripts/popup.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class popup : MonoBehaviour
+{
+    [SerializeField] private UIDocument _uiDocument;
+    private Button goOusYes;
+    private Button goOusNo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // goOutside();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    void goOutside()
+    {
+        var element = _uiDocument.rootVisualElement.Q("popup");
+        var newVisualElement = new VisualElement();
+        newVisualElement.AddToClassList("goOutside");
+        newVisualElement.name = "goOutside";
+
+        var yesButton = new Button();
+        yesButton.AddToClassList("Yes");
+        yesButton.name = "goOutsideYes";
+        var noButton = new Button();
+        noButton.AddToClassList("No");  
+        noButton.name = "goOutsideNo";      
+        newVisualElement.Add(yesButton);
+        newVisualElement.Add(noButton);
+        element.Add(newVisualElement);
+
+        yesButton.clicked += clickedYes;
+        noButton.clicked += clickedNo;
+    }
+
+    void clickedYes()
+    {
+        Debug.Log("Yes");
+    }
+    void clickedNo()
+    {
+        var element = _uiDocument.rootVisualElement.Q("goOutside");
+        element.RemoveFromHierarchy();
+    }
+}
